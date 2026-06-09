af2fd91fb52742e04b57183302114713f0456fc8
sqooid <github@sl.thesqooid.com>
2026-06-09 18:59:17 +1000

    set default api urls

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,10 @@
 class AppConfig {
+  static const bool _isRelease = bool.fromEnvironment('dart.vm.product');
+
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.0.11:8787',
+    defaultValue:
+        _isRelease ? 'https://echolearn-api.thesqooid.com' : 'http://192.168.0.11:8787',
   );
 
   static String get translateUrl => '$apiBaseUrl/translate';
